6840f8de61965b50120a2b83a8fe3a88fe2577cf
Yoshihiro Tanaka <contact@cordea.jp>
2021-11-13 11:27:11 +0900

    draw text

--- lib/home.dart
+++ lib/home.dart
@@ -7,6 +7,44 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
+      body: AspectRatio(
+        aspectRatio: 16 / 9,
+        child: CustomPaint(
+          painter: _CustomPainter(
+            text: 'Flutter Demo',
+            style: Theme.of(context).textTheme.headline1!,
+          ),
+        ),
+      ),
     );
+  }
+}
+
+class _CustomPainter extends CustomPainter {
+  _CustomPainter({
+    required String text,
+    required TextStyle style,
+  }) : _painter = TextPainter(
+          text: TextSpan(text: text, style: style),
+          textDirection: TextDirection.ltr,
+        );
+
+  final TextPainter _painter;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    _painter.layout();
+    _painter.paint(
+      canvas,
+      Offset(
+        (size.width / 2) - (_painter.width / 2),
+        (size.height / 2) - (_painter.height / 2),
+      ),
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
