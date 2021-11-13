06bc490a172691a1a092fe0ecaf196e96fe85a07
Yoshihiro Tanaka <contact@cordea.jp>
2021-11-13 13:15:50 +0900

    animate

--- lib/home.dart
+++ lib/home.dart
@@ -2,21 +2,75 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late CurvedAnimation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.linearToEaseOut,
+      reverseCurve: Curves.easeInToLinear,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animation.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: AspectRatio(
-        aspectRatio: 16 / 9,
-        child: CustomPaint(
-          painter: _CustomPainter(
-            text: 'Flutter Demo',
-            style: Theme.of(context).textTheme.headline1!,
-            progress: 0.8,
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: AnimatedBuilder(
+                animation: _animation,
+                builder: (context, _) {
+                  return CustomPaint(
+                    painter: _CustomPainter(
+                      text: 'Flutter Demo',
+                      style: Theme.of(context).textTheme.headline1!,
+                      progress: _animation.value,
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                if (_controller.isCompleted) {
+                  _controller.reverse();
+                  return;
+                }
+                _controller.forward();
+              },
+              child: const Text('Start'),
+            ),
+          ],
         ),
       ),
     );
@@ -25,7 +79,7 @@ class HomePage extends StatelessWidget {
 
 class _CustomPainter extends CustomPainter {
   _CustomPainter({
-    required String text,
+    required this.text,
     required TextStyle style,
     required this.progress,
   })  : _foregroundPainter = TextPainter(
@@ -46,6 +100,7 @@ class _CustomPainter extends CustomPainter {
   static const _primaryColor = Color(0xff004d40);
   static const _accentColor = Color(0xffffcdd2);
 
+  final String text;
   final double progress;
   final TextPainter _foregroundPainter;
   final TextPainter _backgroundPainter;
@@ -88,6 +143,9 @@ class _CustomPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    if (oldDelegate is! _CustomPainter) {
+      return false;
+    }
+    return oldDelegate.progress != progress || oldDelegate.text != text;
   }
 }
