cb47cb50ed0bd17d9bc3bc572c7363a578f47424
Yoshihiro Tanaka <contact@cordea.jp>
2021-11-13 12:54:52 +0900

    update

--- lib/home.dart
+++ lib/home.dart
@@ -15,6 +15,7 @@ class HomePage extends StatelessWidget {
           painter: _CustomPainter(
             text: 'Flutter Demo',
             style: Theme.of(context).textTheme.headline1!,
+            progress: 0.8,
           ),
         ),
       ),
@@ -26,6 +27,7 @@ class _CustomPainter extends CustomPainter {
   _CustomPainter({
     required String text,
     required TextStyle style,
+    required this.progress,
   })  : _foregroundPainter = TextPainter(
           text: TextSpan(
             text: text,
@@ -41,11 +43,10 @@ class _CustomPainter extends CustomPainter {
           textDirection: TextDirection.ltr,
         )..layout();
 
-  static const _clipRadius = 100.0;
-
   static const _primaryColor = Color(0xff004d40);
   static const _accentColor = Color(0xffffcdd2);
 
+  final double progress;
   final TextPainter _foregroundPainter;
   final TextPainter _backgroundPainter;
 
@@ -56,31 +57,33 @@ class _CustomPainter extends CustomPainter {
       Paint()..color = _primaryColor,
     );
 
-    final path = Path()
-      ..addOval(
-        Rect.fromCircle(
-          center: Offset(size.width / 2, size.height / 2),
-          radius: _clipRadius,
-        ),
-      );
+    final progress = this.progress * 2;
+    final Path path;
+    if (progress > 1) {
+      final p = progress - 1;
+      path = Path()
+        ..moveTo(size.width, size.height)
+        ..lineTo(size.width, 0)
+        ..lineTo(size.width * (1 - p), 0)
+        ..lineTo(0, size.height * (1 - p))
+        ..lineTo(0, size.height)
+        ..close();
+    } else {
+      path = Path()
+        ..moveTo(size.width, size.height)
+        ..lineTo(size.width, size.height * (1 - progress))
+        ..lineTo(size.width * (1 - progress), size.height)
+        ..close();
+    }
     canvas.drawPath(path, Paint()..color = _accentColor);
-    _backgroundPainter.paint(
-      canvas,
-      Offset(
-        (size.width / 2) - (_foregroundPainter.width / 2),
-        (size.height / 2) - (_foregroundPainter.height / 2),
-      ),
-    );
 
+    final offset = Offset(
+      (size.width / 2) - (_foregroundPainter.width / 2),
+      (size.height / 2) - (_foregroundPainter.height / 2),
+    );
+    _backgroundPainter.paint(canvas, offset);
     canvas.clipPath(path);
-
-    _foregroundPainter.paint(
-      canvas,
-      Offset(
-        (size.width / 2) - (_foregroundPainter.width / 2),
-        (size.height / 2) - (_foregroundPainter.height / 2),
-      ),
-    );
+    _foregroundPainter.paint(canvas, offset);
   }
 
   @override
